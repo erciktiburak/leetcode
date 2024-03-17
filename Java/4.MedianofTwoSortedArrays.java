5ace76179b81cc9571b200da16394e8770f86526
Burak Erçıktı <burakercikti@outlook.com>
2024-03-17 22:39:11 +0300

    4. Median Of Two Sorted Arrays with java

---
+++ Java/4.MedianofTwoSortedArrays.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] merged = new int[n1 + n2];
+        
+        int i = 0, j = 0, k = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+        
+        while (i < n1) {
+            merged[k++] = nums1[i++];
+        }
+        
+        while (j < n2) {
+            merged[k++] = nums2[j++];
+        }
+        
+        if ((n1 + n2) % 2 == 0) {
+            return (double)(merged[(n1 + n2) / 2 - 1] + merged[(n1 + n2) / 2]) / 2;
+        } else {
+            return (double)merged[(n1 + n2) / 2];
+        }
+    }
+}
