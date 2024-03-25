5ead0b6511fa20036820a819d37f0cc7f4a45e23
Burak Erçıktı <burakercikti@outlook.com>
2024-03-25 22:32:14 +0300

    4Sum with Java

---
+++ Java/18.4sum.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 4) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicates
+            }
+            for (int j = i + 1; j < nums.length - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue; // Skip duplicates
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            ++left; // Skip duplicates
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            --right; // Skip duplicates
+                        }
+                        ++left;
+                        --right;
+                    } else if (sum < target) {
+                        ++left;
+                    } else {
+                        --right;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
